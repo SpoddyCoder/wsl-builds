058e7542d9829ef7facb4715be18896d6d0cc67e
SpoddyCoder <paul@spoddycoder.com>
2025-07-11 23:18:34 +0100

    Fix sub-directory issue

--- ai-basics/install.sh
+++ ai-basics/install.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
+# Get the directory where this script is located
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 if [ ! -z $INSTALL_CONDA ] && ! (conda --version) > /dev/null 2>&1; then
-    source conda/install_conda.sh
+    source ${SCRIPT_DIR}/conda/install_conda.sh
     BUILD_UPDATED=true
 fi
 
 if [ ! -z $INSTALL_CUDA124 ] && [ ! -f /etc/apt/preferences.d/cuda-repository-pin-600 ]; then
-    source cuda/install_cuda12-4.sh
+    source ${SCRIPT_DIR}/cuda/install_cuda12-4.sh
     BUILD_UPDATED=true
 fi
