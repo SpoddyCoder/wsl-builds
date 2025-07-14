9e136653fafdb9f15567f562af40fbef0754800b
SpoddyCoder <paul@spoddycoder.com>
2025-07-14 01:04:13 +0100

    Fix bug when qol is not only component

--- devops-aws/install_awscli.sh
+++ devops-aws/install_awscli.sh
@@ -7,11 +7,15 @@ sudo apt-get install -y curl unzip
 
 printInfo "Downloading and installing AWS CLI v2..."
 getFile "awscli-exe-linux-x86_64.zip" "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" "/tmp" awscli_zip
+
+# Save current directory before changing to /tmp
+pwd=$(pwd)
 cd /tmp
 unzip "$awscli_zip"
 sudo ./aws/install
 rm -rf aws/
 cleanupGetFiles
+cd "$pwd"
 
 printInfo "AWS CLI installed successfully..."
 aws --version
